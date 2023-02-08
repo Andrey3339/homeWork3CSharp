9504811b0451f2c244d92f83200e0b89ac6d6a32
Andrey Shevchuk <Shevchuk339@mail.ru>
2023-02-08 20:09:15 +0500

    add task19

---
+++ task19/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.Write("Введите 5-значное число: ");
+string s = Console.ReadLine();
+if ((s.Trim()).Length != 5) 
+{
+    Console.WriteLine("Вы ввели не 5-значное число, введите 5-значное число: ");
+    s = Console.ReadLine();
+}
+string s2 = s.Trim();
+if(s2[0] == s2[4] && s2[1] == s2[3])
+{
+    Console.WriteLine($"Введенное число {s2} является палиндромом.");
+}
+else 
+    Console.WriteLine($"Введенное число {s2} не является палиндромом.");
+
