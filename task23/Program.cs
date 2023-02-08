d7cb6fb81644972fe0f62ffc736891030faf2374
Andrey Shevchuk <Shevchuk339@mail.ru>
2023-02-08 20:27:32 +0500

    add task23

---
+++ task23/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+Console.Clear();
+Console.Write("Введите натуральное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"куб чисел от 1 до {n} равен: ");
+for(int i = 1; i <= n; i++)
+{
+    Console.Write(i*i*i + " ");
+
+}
+
